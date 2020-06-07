50c26bdf5160c83d44a4bc8a9e38dc19bceabc47
chinniNeelamaiah <65772969+chinniNeelamaiah@users.noreply.github.com>
2020-06-07 13:40:46 +0530

    home and button js included

---
+++ smartWatch.js
@@ -0,0 +1,19 @@
+var date = new Date();
+var daysInWeek=["Sunday","Monday","Tuesday","Wednesday","<b>Thursday","<b>Friday","Saturday"]
+var day=date.getDay();
+var today = daysInWeek[day];
+var hours =date.getHours();
+var min =date.getMinutes();
+function home(){
+document.getElementById("screen").innerText=(today+" "+hours+":"+min);
+
+}
+
+
+function message(){
+document.getElementById("screen").innerText="Messages"+" "+hours+":"+min+" \n\n Hola!....This is message";
+
+}
+
+
+
